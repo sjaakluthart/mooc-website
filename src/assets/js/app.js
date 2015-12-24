9f7d8a6a8231157f3de0cfad72064c6257d8a0ce
Sjaak Luthart <mail@sjaakluthart.nl>
2015-12-24 19:47:52 +0100

    fixed code style issues

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,41 +6,41 @@ $(document).ready(function(){
         triggerElement: '#pre-production1'
     })
         .setClassToggle('.fixed', 'active')
-        .addIndicators({name: 'pre-production1'}) // add indicators (requires plugin)
+        .addIndicators({name: 'pre-production1'}) // Add indicators (requires plugin)
         .addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: '#pre-production1'
     })
         .setClassToggle('.pin-top', 'pinned')
-        .addIndicators({name: 'pre-production1'}) // add indicators (requires plugin)
+        .addIndicators({name: 'pre-production1'}) // Add indicators (requires plugin)
         .addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: '#pre-production2'
     })
         .setClassToggle('#pre-production2 ~ h2', 'pinned')
-        .addIndicators({name: 'pre-production2'}) // add indicators (requires plugin)
+        .addIndicators({name: 'pre-production2'}) // Add indicators (requires plugin)
         .addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: '#pre-production3'
     })
         .setClassToggle('.fade-in', 'active')
-        .addIndicators({name: 'pre-production3'}) // add indicators (requires plugin)
+        .addIndicators({name: 'pre-production3'}) // Add indicators (requires plugin)
         .addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: '#pre-production4'
     })
         .setClassToggle('#pre-production2 ~ h2', 'hide')
-        .addIndicators({name: 'pre-production4'}) // add indicators (requires plugin)
+        .addIndicators({name: 'pre-production4'}) // Add indicators (requires plugin)
         .addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: '#pre-production5'
     })
         .setClassToggle('#pre-production5 ~ h2', 'pinned')
-        .addIndicators({name: 'pre-production5'}) // add indicators (requires plugin)
+        .addIndicators({name: 'pre-production5'}) // Add indicators (requires plugin)
         .addTo(controller);
 });
