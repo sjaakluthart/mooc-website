efe6c6a069f80820238f62a62ec8ae72690d205a
Sjaak Luthart <mail@sjaakluthart.nl>
2016-02-14 15:59:42 +0100

    added scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,36 @@ var gulp = require('gulp'),
     stylish = require('gulp-jscs-stylish'),
     scsslint = require('gulp-scss-lint'),
     tar = require('gulp-tar'),
-    sources;
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    header = require('gulp-header'),
+    sources,
+    scripts,
+    bowerBanner;
 
 sources = [
     'gulpfile.js',
     'src/**/*.js',
     '!node_modules/**/*.js',
+    '!bower_components/**/*.js',
     '!src/**/*.min.js'
 ];
+
+scripts = [
+    './bower_components/jquery/dist/jquery.min.js',
+    './bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+    './bower_components/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js',
+    './bower_components/elevator.js/elevator.min.js',
+];
+
+bowerBanner = ['/**',
+  ' * Bower Components',
+  ' * jQuery 2.2.0 - https://github.com/jquery/jquery',
+  ' * ScrollMagic.js - https://github.com/janpaepke/ScrollMagic',
+  ' * Elevator.js - https://github.com/tholman/elevator.js',
+  ' */',
+  ''].join('\n');
 
 gulp.task('jscs', function() {
     return gulp.src(sources)
@@ -45,6 +67,13 @@ gulp.task('sass', function() {
     return sass('src/assets/sass/**/style.scss', {style: 'compressed'})
         .on('error', sass.logError)
         .pipe(gulp.dest('src/assets/stylesheets'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(scripts)
+  .pipe(concat('scripts.js'))
+  .pipe(header(bowerBanner))
+  .pipe(gulp.dest('./src/assets/'));
 });
 
 gulp.task('watch', function() {
