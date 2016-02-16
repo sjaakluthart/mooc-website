ecfc9bfdb6ad2005dd4cd8580821c1cc44c4c8f6
Sjaak Luthart <sjaak@kickorstick.com>
2016-02-16 16:38:02 +0100

    added click events for menu

--- src/assets/click.js
+++ src/assets/click.js
@@ -5,5 +5,24 @@ $(document).ready(function(){
     duration: 5500
   });
 
+  $('nav li:nth-of-type(1)').click(function(event) {
+    $(window).scrollTo('#pre-production', 1000);
+  });
 
-}):
+  $('nav li:nth-of-type(2)').click(function(event) {
+    $(window).scrollTo('#pre-production22', 1000);
+  });
+
+  $('nav li:nth-of-type(3)').click(function(event) {
+    $(window).scrollTo('#production1', 1000);
+  });
+
+  $('nav li:nth-of-type(4)').click(function(event) {
+    $(window).scrollTo('#execution', 1000);
+  });
+
+  $('nav li:nth-of-type(5)').click(function(event) {
+    console.log('click5');
+  });
+
+});
