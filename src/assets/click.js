4858033d3d2b83cb72fc85d9c26f254bbec36178
Sjaak Luthart <mail@sjaakluthart.nl>
2016-02-27 16:02:13 +0100

    added overview

--- src/assets/click.js
+++ src/assets/click.js
@@ -25,4 +25,33 @@ $(document).ready(function(){
     $(window).scrollTo('#evaluation', 1000);
   });
 
+  $('.overview').click(function(event) {
+    $('#overview').toggleClass('active');
+  });
+
+  $('#overview1').click(function(event) {
+    $('#overview').toggleClass('active');
+    $(window).scrollTo('#pre-production', 1000);
+  });
+
+  $('#overview2').click(function(event) {
+    $('#overview').toggleClass('active');
+    $(window).scrollTo('#pre-production22', 1000);
+  });
+
+  $('#overview3').click(function(event) {
+    $('#overview').toggleClass('active');
+    $(window).scrollTo('#production1', 1000);
+  });
+
+  $('#overview4').click(function(event) {
+    $('#overview').toggleClass('active');
+    $(window).scrollTo('#execution', 1000);
+  });
+
+  $('#overview5').click(function(event) {
+    $('#overview').toggleClass('active');
+    $(window).scrollTo('#evaluation', 1000);
+  });
+
 });
